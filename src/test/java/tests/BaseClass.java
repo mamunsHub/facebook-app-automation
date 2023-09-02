0ce9ff07aa3e2a7b3bdb2e9709554cbae449a97f
Abdullah Al Mamun <mamunabdullah.cse007@gmail.com>
2023-09-02 18:03:11 +0600

    Added dependencies

---
+++ src/test/java/tests/BaseClass.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+
+    @BeforeTest
+    public void setup() {
+
+    }
+
+    @AfterTest
+    public void teardown() {
+
+    }
+
+
+}
